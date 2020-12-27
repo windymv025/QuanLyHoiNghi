11e47e30308ac840ac720b834e0dbe82a64031ca
DangThanh2010 <57797011+DangThanh2010@users.noreply.github.com>
2020-12-27 22:19:55 +0700

    Add interface for LoiXemHoiNghiDaDangKy

--- Project/QuanLyHoiNghi/QuanLyHoiNghi/LoiXemHNDDKWindow.xaml.cs
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/LoiXemHNDDKWindow.xaml.cs
@@ -10,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace QuanLyHoiNghi
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoiXemHNDDKWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoiXemHNDDKWindow : Window
     {
-        public MainWindow()
+        public LoiXemHNDDKWindow()
         {
             InitializeComponent();
         }
 
-        private void hoiNghiDangKyBtn_Click(object sender, RoutedEventArgs e)
+        private void trangChuBtn_Click(object sender, RoutedEventArgs e)
         {
-            LoiXemHNDDKWindow HNDDangKy = new LoiXemHNDDKWindow();
-            HNDDangKy.Show();
+            MainWindow Main = new MainWindow();
+            Main.Show();
             this.Close();
         }
     }
