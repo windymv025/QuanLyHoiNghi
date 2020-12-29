e107fd5e2da139503b826a857029b8cc556b8c65
TranqBuong <trantrang0902paxxa@gmail.com>
2020-12-29 22:56:27 +0700

    Add interface for QuanLyHoiNghi

---
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/QuanLyHoiNghiWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyHoiNghi
+{
+    /// <summary>
+    /// Interaction logic for QuanLyHoiNghiWindow.xaml
+    /// </summary>
+    public partial class QuanLyHoiNghiWindow : Window
+    {
+        public QuanLyHoiNghiWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void trangChuBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Main = new MainWindow();
+            Main.Show();
+            this.Close();
+        }
+
+        private void timKiemBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+        private void themBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
