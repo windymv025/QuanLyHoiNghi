897028ce3f92f731c31bf5dd92e56e36759600ec
ThinhNguyenHuu <nguyenhuuthinh13112000@gmail.com>
2020-12-31 14:35:34 +0700

    Add ChiTietHoiNghiWindow and ChiTietHoiNghiViewModel

---
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/ChiTietHoiNghiWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using QuanLyHoiNghi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyHoiNghi
+{
+    /// <summary>
+    /// Interaction logic for ChiTietHoiNghi.xaml
+    /// </summary>
+    public partial class ChiTietHoiNghi : Window
+    {
+        private ChiTietHoiNghiViewModel viewModel;
+
+        public ChiTietHoiNghi()
+        {
+            InitializeComponent();
+            ChiTietHoiNghiViewModel viewModel = new ChiTietHoiNghiViewModel(null, null);
+            this.viewModel = viewModel;
+            this.DataContext = viewModel;
+        }
+
+        private void btnTrangChu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHoiNghiDangKY_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnQuanLyHoiNghi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTaiKhoan_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
