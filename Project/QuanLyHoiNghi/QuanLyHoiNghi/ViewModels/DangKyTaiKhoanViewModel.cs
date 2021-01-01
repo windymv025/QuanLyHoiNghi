f0c68fedea817349f78e05c7a3d9737c58f23c9e
windymv025 <windymv025@users.noreply.github.com>
2021-01-01 17:17:00 +0700

    update giao diện đăng ký đăng nhập, thêm tính năng mã hóa mật khẩu

--- Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangKyTaiKhoanViewModel.cs
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangKyTaiKhoanViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +23,7 @@ namespace QuanLyHoiNghi.ViewModels
                 user.IDUSER = idMax + 1;
                 user.TENUSER = ten;
                 user.USERNAME = tenDangNhap;
-                user.PASSWORD = matKhau;
+                user.PASSWORD = this.maHoaMatKhau(matKhau);
                 user.EMAIL = email;
                 user.LOAIUSER = loaiUser;
                 user.HINHANH = "Images/user_example.png";
@@ -42,6 +43,25 @@ namespace QuanLyHoiNghi.ViewModels
                            select user.USERNAME;
                 return list.ToList();
             }
+        }
+        
+        public string maHoaMatKhau(string matKhau)
+        {
+            //Tạo MD5 
+            MD5 mh = MD5.Create();
+            //Chuyển kiểu chuổi thành kiểu byte
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(matKhau);
+            //mã hóa chuỗi đã chuyển
+            byte[] hash = mh.ComputeHash(inputBytes);
+            //tạo đối tượng StringBuilder (làm việc với kiểu dữ liệu lớn)
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            //nếu bạn muốn các chữ cái in thường thay vì in hoa thì bạn thay chữ "X" in hoa trong "X2" thành "x"
+            return sb.ToString();
         }
     }
 }
--- Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangNhapViewModel.cs
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangNhapViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,11 @@ namespace QuanLyHoiNghi.ViewModels
 {
     class DangNhapViewModel
     {
-        public static USER User = null;
+        public  USER User { get; set; }
 
         public bool kiemTraUser(string username, string password)
         {
+            string hashPass = this.maHoaMatKhau(password);
             using (DBQuanLiHoiNghiEntities db = new DBQuanLiHoiNghiEntities())
             {
                 USER user = null;
@@ -23,12 +25,30 @@ namespace QuanLyHoiNghi.ViewModels
                 if (user == null)
                     return false;
 
-                if (user.PASSWORD != password)
+                if (user.PASSWORD != hashPass)
                     return false;
 
                 User = user;
                 return true;
             }
+        }
+        public string maHoaMatKhau(string matKhau)
+        {
+            //Tạo MD5 
+            MD5 mh = MD5.Create();
+            //Chuyển kiểu chuổi thành kiểu byte
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(matKhau);
+            //mã hóa chuỗi đã chuyển
+            byte[] hash = mh.ComputeHash(inputBytes);
+            //tạo đối tượng StringBuilder (làm việc với kiểu dữ liệu lớn)
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            //nếu bạn muốn các chữ cái in thường thay vì in hoa thì bạn thay chữ "X" in hoa trong "X2" thành "x"
+            return sb.ToString();
         }
     }
 }
