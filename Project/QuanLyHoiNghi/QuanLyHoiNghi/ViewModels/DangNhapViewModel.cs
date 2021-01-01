ec1bab8e470c5263de80867f746e222ad48aada2
windymv025 <windymv025@users.noreply.github.com>
2021-01-01 17:31:45 +0700

    update ma hoa pass

--- Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangNhapViewModel.cs
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/DangNhapViewModel.cs
@@ -15,7 +15,6 @@ namespace QuanLyHoiNghi.ViewModels
 
         public bool kiemTraUser(string username, string password)
         {
-            string hashPass = this.maHoaMatKhau(password);
             using (DBQuanLiHoiNghiEntities db = new DBQuanLiHoiNghiEntities())
             {
                 USER user = null;
@@ -25,30 +24,12 @@ namespace QuanLyHoiNghi.ViewModels
                 if (user == null)
                     return false;
 
-                if (user.PASSWORD != hashPass)
+                if (user.PASSWORD != password)
                     return false;
 
                 User = user;
                 return true;
             }
-        }
-        public string maHoaMatKhau(string matKhau)
-        {
-            //Tạo MD5 
-            MD5 mh = MD5.Create();
-            //Chuyển kiểu chuổi thành kiểu byte
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(matKhau);
-            //mã hóa chuỗi đã chuyển
-            byte[] hash = mh.ComputeHash(inputBytes);
-            //tạo đối tượng StringBuilder (làm việc với kiểu dữ liệu lớn)
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < hash.Length; i++)
-            {
-                sb.Append(hash[i].ToString("X2"));
-            }
-            //nếu bạn muốn các chữ cái in thường thay vì in hoa thì bạn thay chữ "X" in hoa trong "X2" thành "x"
-            return sb.ToString();
         }
     }
 }
