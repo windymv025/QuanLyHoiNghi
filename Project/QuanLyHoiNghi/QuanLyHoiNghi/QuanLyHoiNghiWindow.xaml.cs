0b186e09ad2c23becde34d064d549dccffc98c7c
TranqBuong <trantrang0902paxxa@gmail.com>
2021-01-03 14:10:35 +0700

    Update QuanLyHoiNghi

--- Project/QuanLyHoiNghi/QuanLyHoiNghi/QuanLyHoiNghiWindow.xaml.cs
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/QuanLyHoiNghiWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyHoiNghi.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-using QuanLyHoiNghi.ViewModels;
+using QuanLyHoiNghi.Model;
 
 namespace QuanLyHoiNghi
 {
@@ -21,11 +22,27 @@ namespace QuanLyHoiNghi
     /// </summary>
     public partial class QuanLyHoiNghiWindow : Window
     {
+        QuanLyHoiNghiViewModel viewModel;
         public QuanLyHoiNghiWindow()
         {
             InitializeComponent();
-        }
+            viewModel = new QuanLyHoiNghiViewModel();
+            viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetAllHoiNghiQuanLy();
+            int total = viewModel.ListHoiNghi.Count;
+            viewModel.PagingInfo = new PagingInfoOfHNQL(4, total);
 
+            loadPageHoiNghiQuanLy(1);
+        }
+        private void loadPageHoiNghiQuanLy(int page)
+        {
+            var result = viewModel.loadPage(page, viewModel.PagingInfo.ItemInPerPage);
+            lvDanhSachHoiNghiQL.ItemsSource = result;
+            if (viewModel.PagingInfo.TotalPage == 0)
+            {
+                viewModel.PagingInfo.CurrentPage = 0;
+            }
+            trangHienTaiTxt.Text = $"{viewModel.PagingInfo.CurrentPage}/{viewModel.PagingInfo.TotalPage}";
+        }
 
         private void trangChuBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -67,13 +84,150 @@ namespace QuanLyHoiNghi
         }
         private void timKiemBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            
+
+            if (timKiem_grid.Visibility == Visibility.Collapsed)
+            {
+                timKiem_grid.Visibility = Visibility.Visible;
+                sapXep_grid.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                timKiem_grid.Visibility = Visibility.Collapsed;
+                sapXep_grid.Visibility = Visibility.Visible;
+
+                tenHoiNghiTextBox.Text = "";
+                diadiemTextBox.Text = "";
+
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetAllHoiNghiQuanLy();
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
         }
+       
+        private void prevBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            loadPageHoiNghiQuanLy(viewModel.PagingInfo.CurrentPage - 1);
+        }
+
+        private void nextBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            loadPageHoiNghiQuanLy(viewModel.PagingInfo.CurrentPage + 1);
+        }
+
+        private void loaiSapXepCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (loaiSapXepCB.SelectedIndex == 0)
+            {
+                if (hinhThucSapXepCB.SelectedIndex == 0)
+                {
+                    viewModel.sapXepHoiNghiTangDanTheoTen();
+                    loadPageHoiNghiQuanLy(1);
+                }
+                else
+                {
+                    viewModel.sapXepHoiNghiTangDanTheoThoiGian();
+                    loadPageHoiNghiQuanLy(1);
+                }
+            }
+            else
+            {
+                if (hinhThucSapXepCB.SelectedIndex == 0)
+                {
+                    viewModel.sapXepHoiNghiGiamDanTheoTen();
+                    loadPageHoiNghiQuanLy(1);
+                }
+                else
+                {
+                    viewModel.sapXepHoiNghiGiamDanTheoThoiGian();
+                    loadPageHoiNghiQuanLy(1);
+                }
+            }
+        }
+
+        private void tenHoiNghiTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string name = tenHoiNghiTextBox.Text.Trim();
+            string place = diadiemTextBox.Text.Trim();
+            DateTime date = ngayBatDauDatePicker.SelectedDate.GetValueOrDefault();
+            DateTime defaultDate = new DateTime();
+
+            if (name != "" || place != "" || date != defaultDate)
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetResultSearchHoiNghiQuanLy(name, place, date);
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+            else
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetAllHoiNghiQuanLy();
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+        }
+
+        private void diadiemTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string name = tenHoiNghiTextBox.Text.Trim();
+            string place = diadiemTextBox.Text.Trim();
+            DateTime date = ngayBatDauDatePicker.SelectedDate.GetValueOrDefault();
+            DateTime defaultDate = new DateTime();
+
+            if (name != "" || place != "" || date != defaultDate)
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetResultSearchHoiNghiQuanLy(name, place, date);
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+            else
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetAllHoiNghiQuanLy();
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+        }
+
+        private void ngayBatDauDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string name = tenHoiNghiTextBox.Text.Trim();
+            string place = diadiemTextBox.Text.Trim();
+            DateTime date = ngayBatDauDatePicker.SelectedDate.GetValueOrDefault();
+            DateTime defaultDate = new DateTime();
+
+            if (name != "" || place != "" || date != defaultDate)
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetResultSearchHoiNghiQuanLy(name, place, date);
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+            else
+            {
+                viewModel.ListHoiNghi = QuanLyHoiNghiDAO.GetAllHoiNghiQuanLy();
+                viewModel.PagingInfo = new PagingInfoOfHNQL(4, viewModel.ListHoiNghi.Count);
+                loadPageHoiNghiQuanLy(1);
+            }
+        }
+
+
+        private void ListViewItem_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            ListViewItem item = (ListViewItem)sender;
+            item.IsSelected = false;
+        }
+
+        private void ListViewItem_PreviewGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            ListViewItem item = (ListViewItem)sender;
+            item.IsSelected = true;
+        }
+
         private void themBtn_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            //
         }
 
-       
+        private void chinhSuaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
     }
 }
---
+++ Project/QuanLyHoiNghi/QuanLyHoiNghi/ViewModels/QuanLyHoiNghiViewModel.cs
@@ -0,0 +1,171 @@
+﻿using QuanLyHoiNghi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHoiNghi.ViewModels
+{
+    public class QuanLyHoiNghiDAO
+    {
+        public static List<HOINGHI> GetAllHoiNghiQuanLy()
+        {
+            List<HOINGHI> list = new List<HOINGHI>();
+            using (DBQuanLiHoiNghiEntities db = new DBQuanLiHoiNghiEntities())
+            {
+
+                var listHoiNghi = from hoiNghi in db.HOINGHIs
+                                  join chitietadmin in db.CHITIETADMINs
+                                  on hoiNghi.IDHN equals chitietadmin.IDHN
+                                  join user in db.USERs
+                                  on chitietadmin.IDUSER equals user.IDUSER 
+                                  where user.LOAIUSER == "1"
+                                  select hoiNghi;
+                list = listHoiNghi.ToList();
+            }
+            return list;
+        }
+
+        public static List<HOINGHI> GetResultSearchHoiNghiQuanLy(string name, string place, DateTime date)
+        {
+            List<HOINGHI> list = new List<HOINGHI>();
+            using (DBQuanLiHoiNghiEntities db = new DBQuanLiHoiNghiEntities())
+            {
+                string sqlHoiNghi = $"select * from HOINGHI where (TENHN LIKE N'%{name}%')";
+                List<HOINGHI> resultHoiNghi = db.HOINGHIs.SqlQuery(sqlHoiNghi).ToList();
+                DateTime defaultDate = new DateTime();
+
+                if (date != defaultDate)
+                {
+                    if (resultHoiNghi.Count <= 0)
+                    {
+                        resultHoiNghi = db.HOINGHIs.ToList();
+                    }
+                    resultHoiNghi = (from item in resultHoiNghi
+                                     where item.THOIGIANBATDAU.Date == date.Date
+                                     select item).ToList();
+                }
+
+                string sqlDiaDiem = $"select* from DIADIEMTOCHUC where (TENDD LIKE N'%{place}%')";
+                var resultDiaDiem = db.DIADIEMTOCHUCs.SqlQuery(sqlDiaDiem);
+
+                if (resultDiaDiem.Count() > 0 && resultHoiNghi.Count > 0)
+                {
+                    var result = from hn in resultHoiNghi
+                                 join dd in resultDiaDiem
+                                 on hn.IDDD equals dd.IDDD
+                                 select hn;
+
+                    list = result.ToList();
+                }
+                else if (resultHoiNghi.Count > 0)
+                {
+                    list = resultHoiNghi;
+                }
+                else if (resultDiaDiem.Count() > 0)
+                {
+                    string sql = $"select hn.IDHN, hn.IDDD, TENHN,hn.MOTANGANHN, hn.MOTACHITIETHN, hn.HINHANH, hn.THOIGIANBATDAU, hn.THOIGIANKETTHUC, hn.SOLUONG from HOINGHI as hn join DIADIEMTOCHUC as dd on hn.IDDD = dd.IDDD where (dd.TENDD LIKE N'%{place}%')";
+                    var result = db.HOINGHIs.SqlQuery(sql);
+                    list = result.ToList();
+                }
+
+                var listHN = from hn in list
+                             join ctam in db.CHITIETADMINs
+                             on hn.IDHN equals ctam.IDHN
+                             join user in db.USERs
+                             on ctam.IDUSER equals user.IDUSER
+                             where user.LOAIUSER == "1"
+                             select hn;
+
+                list = listHN.ToList();
+            }
+            return list;
+        }
+    }
+
+    public class PagingInfoOfHNQL
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int ItemInPerPage { get; set; }
+
+        public PagingInfoOfHNQL(int numberOfItemInPerPage, int total)
+        {
+            if (total > 0)
+            {
+                CurrentPage = 1;
+                ItemInPerPage = numberOfItemInPerPage;
+                TotalPage = total / numberOfItemInPerPage +
+                    ((total % numberOfItemInPerPage) == 0 ? 0 : 1);
+            }
+            else
+            {
+                CurrentPage = 0;
+                TotalPage = 0;
+            }
+
+        }
+    }
+    public class QuanLyHoiNghiViewModel
+    {
+        public PagingInfoOfHNQL PagingInfo { get; set; }
+        public List<HOINGHI> ListHoiNghi { get; set; }
+
+        public List<HOINGHI> loadPage(int pageNumber, int numItemInPage)
+        {
+            List<HOINGHI> result = new List<HOINGHI>();
+            PagingInfo.ItemInPerPage = numItemInPage;
+
+            if (PagingInfo.TotalPage > 0)
+            {
+                if (pageNumber > PagingInfo.TotalPage)
+                    pageNumber = 1;
+                if (pageNumber < 1)
+                    pageNumber = PagingInfo.TotalPage;
+
+                PagingInfo.CurrentPage = pageNumber;
+
+                result = ListHoiNghi.Skip((pageNumber - 1) * PagingInfo.ItemInPerPage).Take(PagingInfo.ItemInPerPage).ToList();
+            }
+            else
+            {
+                PagingInfo.CurrentPage = 0;
+            }
+            return result;
+        }
+
+        public void sapXepHoiNghiTangDanTheoTen()
+        {
+            var list = from hoiNghi in this.ListHoiNghi
+                       orderby hoiNghi.TENHN
+                       select hoiNghi;
+
+            this.ListHoiNghi = list.ToList();
+        }
+        public void sapXepHoiNghiTangDanTheoThoiGian()
+        {
+            var list = from hoiNghi in this.ListHoiNghi
+                       orderby hoiNghi.THOIGIANBATDAU
+                       select hoiNghi;
+
+            this.ListHoiNghi = list.ToList();
+        }
+        public void sapXepHoiNghiGiamDanTheoTen()
+        {
+            var list = from hoiNghi in this.ListHoiNghi
+                       orderby hoiNghi.TENHN descending
+                       select hoiNghi;
+
+            this.ListHoiNghi = list.ToList();
+        }
+        public void sapXepHoiNghiGiamDanTheoThoiGian()
+        {
+            var list = from hoiNghi in this.ListHoiNghi
+                       orderby hoiNghi.THOIGIANBATDAU descending
+                       select hoiNghi;
+
+            this.ListHoiNghi = list.ToList();
+        }
+    }
+}
